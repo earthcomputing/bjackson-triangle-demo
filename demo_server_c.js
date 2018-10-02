cf4e275d9a5f7c651d1004b4e703daea13150507
Bill Jackson <bjackson@earthcomputing.io>
2018-10-01 17:08:03 -0700

    bj-ify demo_server_c.js ; add restful API

--- demo_server_c.js
+++ demo_server_c.js
@@ -1,128 +1,134 @@
+
 const s_port = 3000;
 const c_port = 1337;
 
-var net = require('net');
-
+var last_ait = {};
 var json_data = {};
-var connected = 0 ;
-var c_socket ;
-var express = require('express');
-var path = require('path');
+var connected = 0;
+var c_socket;
 
-var app = express();
-//var server = require('http').Server(app);
+// npm install body-parser express socket.io
 var bodyParser = require('body-parser');
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var net = require('net');
+var path = require('path');
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/demo_cell.html');
-});
-
-io.on('connection', function(socket){
-    console.log('io connected')
-    connected = 1 ;
-  socket.on('aitMessage', function(msg){
-    var message = msg.message ;
-    var port = msg.port ;
-    console.log('AIT ' + port + ' ' + message);
-    c_socket.write( '{ \"port\": \"' + port + '\", \"message\":\"' + message + '\" }')
-  });
-});
-
-app.use('/', express.static(path.join(__dirname, '/')));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/', express.static(path.join(__dirname, '/')));
 
-http.listen(s_port, function(){
-  console.log('listening on *:'+s_port);
+// HTTP request methods: get, head, post, put, delete, trace, options, connect, patch
+
+// http://localhost:3000/ports
+app.get('/ports', function (req, res) {
+    var current_state = JSON.stringify(json_data) + JSON.stringify(last_ait);
+    console.log('GET ports ...', current_state);
+    res.send('GET ports ...' + current_state);
+    // res.status(status).send(body)
 });
 
-//var l = io.listen(server);
+// static char *port_name[NUM_INTERFACES] = { "enp6s0", "enp7s0", "enp8s0", "enp9s0" };
+// http://localhost:3000/port/3
+// http://localhost:3000/port/enp6s0
+app.all('/port/:port_id', function (req, res, next) {
+    console.log('port ...', req.method, req.url, req.params);
+    next();
+})
 
-var data = {"deviceName": "enp6s0",
-        "linkState": "UP",
-        "entlState":"HELLO",
-        "entlCount":"990",
-        "AITMessage":"-none-"
-       };
+app.post('/port/:port_id', function (req, res) {
+    var frame = req.body.frame;
+    console.log('POST port ...',
+        '\nheaders:', req.headers,
+        '\nbody:', req.body,
+        '\nframe:', frame
+    );
+    last_ait[req.params.port_id] = frame;
+    adapterWrite(req.params.port_id, frame);
+    res.send('POST port ...' + JSON.stringify(req.params) + frame);
+});
 
-//l.sockets.on("connection", function(socket) {
-//    console.log('Server connected');
- //   s_socket = socket ;
- //   connected = 1 ;
- //       //    enp2s0Update(socket, data);
-//    earthUpdate(socket, data);
+function adapterWrite(port, message) {
+    c_socket.write('{ \"port\": \"' + port + '\", \"message\":\"' + message + '\" }')
+}
 
-//    socket.on("aitMessage", function(data){
-//    console.log("in AIT handler");
-//    earthAITMessage(socket, data);
-//});
-//});
+app.get('/port/:port_id', function (req, res) {
+    console.log('GET port ...', req.params);
+    var message = last_ait[req.params.port_id];
+    res.send('GET port ...' + JSON.stringify(req.params) + message);
+});
 
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/demo_cell.html');
+});
 
-//update function for earth-connect update
-function earthUpdate (socket, data) {
+// aitMessage { port message }
+io.on('connection', function (socket) {
+    console.log('io connected');
+    connected = 1;
+    // sendPacket - send buttom
+    socket.on('aitMessage', function (msg) {
+        var message = msg.message;
+        var port = msg.port;
+        console.log('AIT ' + port + ' ' + message);
+        last_ait[port] = message;
+        c_socket.write('{ \"port\": \"' + port + '\", \"message\":\"' + message + '\" }')
+    });
+});
+
+http.listen(s_port, function () {
+    console.log('listening on *:' + s_port);
+});
+
+var data = {
+    "deviceName" : "enp6s0",
+    "linkState" : "UP",
+    "entlState" : "HELLO",
+    "entlCount" : "990",
+    "AITMessage" : "-none-"
+};
+
+// update function for earth-connect update
+function earthUpdate(socket, data) {
     console.log(data);
     io.emit("earth-update", data);
-} 
-
-//var server = net.createServer(function(socket) {
-//    console.log('Target connected');
-//    s_socket = socket ;
-//    connected = 1 ;
-//});
+}
 
 function isBlank(str) {
     return (!str || /^\s*$/.test(str));
 }
 
-var client = net.createServer(function(socket) {
-    c_socket = socket ;
+var client = net.createServer(function (socket) {
+    c_socket = socket;
     console.log('Client connected');
-
-    //socket.write('Echo server\r\n');
-    //socket.pipe(socket);
 
     socket.on('data', function (data) {
         data = data.toString();
-                  //console.log('data:'+data);
-        var jd = data.split("\n") ;
-        for( var i = 0, len = jd.length; i < len; i++ ) {
-            var d = jd[i] ;
-
-            if(!isBlank(d)) {
-                //console.log('split:'+d);
-
+        var jd = data.split("\n");
+        for (var i = 0, len = jd.length; i < len; i++) {
+            var d = jd[i];
+            if (!isBlank(d)) {
                 try {
-                    var obj = JSON.parse(d) ;
-                    if( obj ) {
-                        console.log( "entlCount:", obj.entlCount) ;
-                        if( obj.machineName ) {
-                            json_data[obj.machineName] = d ;
-                            //console.log( 'machine['+obj.machineName+'] = '+json_data[obj.machineName]) ;
-                            if( connected ) {
+                    var obj = JSON.parse(d);
+                    if (obj) {
+                        console.log( "entlCount:", obj.entlCount);
+                        if (obj.machineName) {
+                            json_data[obj.machineName] = d;
+                            if (connected) {
+// I/O to spinner (demo_cell.html)
                                 io.emit('earth-update', d);
-                                //s_socket.write(d+'\n') ;
-                                console.log('earth-update '+d);
+                                console.log('earth-update ' + d);
                             }
-                        }                
+                        }
                     }
                 } catch(e) {
-                    console.log('error:'+e) ;
+                    console.log('error:' + e);
                 }
-
             }
-
-
         }
-
    });
-
 });
 
-
-
-
-//server.listen(port, '127.0.0.1');
-//server.listen(s_port);
 client.listen(c_port, '127.0.0.1');
