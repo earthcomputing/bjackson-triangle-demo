0e9f6b059e9a050717b26915c1ba1343895b5a3d
Bill Jackson <bjackson@earthcomputing.io>
2018-10-26 23:02:31 -0700

    add update script

---
+++ do-update.sh
@@ -0,0 +1,22 @@
+#!/bin/csh -f
+
+set alice = 172.16.1.67
+set bob = 172.16.1.40
+set carol = 172.16.1.105
+
+# set USER_NAME = `git config user.email`
+# env GIT_AUTHOR_EMAIL=${USER_NAME} 
+
+set URL = ''
+if ( $#argv > 0 ) then
+    set URL = "https://$1@github.com/earthcomputing/bjackson-triangle-demo.git"
+endif
+
+set body = "cd /home/demouser/earthcomputing/triangle-demo && git pull ${URL} && make -f Makefile.linux"
+
+echo "$body"
+
+foreach one ( ${alice} ${bob} ${carol} )
+    ssh -t demouser@${one} "${body}"
+end
+
